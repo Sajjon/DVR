613995a0f6f45464dc858b883db26e034bf9003c
Honza Dvorsky <honza@swiftkey.com>
2015-06-27 23:38:57 +0100

    almost there - trying to test that nil response calls a fatal error instead of crashing elsewhere

--- DVR/Tests/SessionTests.swift
+++ DVR/Tests/SessionTests.swift
@@ -30,4 +30,45 @@ class SessionTests: XCTestCase {
         task?.resume()
         waitForExpectationsWithTimeout(1, handler: nil)
     }
+    
+    func testSessionReturnsNilResponse() {
+        session.recordingEnabled = false
+        let expectation = expectationWithDescription("Network")
+        
+        let backingSession = SessionReturningNilResponse()
+        let testSession = Session(cassetteName: "nilly", backingSession: backingSession)
+        let testRequest = NSURLRequest(URL: NSURL(string: "https://nil.ly")!)
+
+        let task = testSession.dataTaskWithRequest(testRequest) { data, response, error in
+            
+            //something like 
+//            XCTAssertThrowsFatalError() - ensure that fatal error was called.
+            expectation.fulfill()
+        }
+        task?.resume()
+        waitForExpectationsWithTimeout(1, handler: nil)
+    }
+}
+
+class SessionNoopTask: NSURLSessionDataTask {
+    
+    let completion: () -> ()
+    
+    init(completion: () -> ()) {
+        self.completion = completion
+    }
+    
+    override func resume() {
+        //nada, just complete
+        self.completion()
+    }
+}
+
+class SessionReturningNilResponse: NSURLSession {
+    override func dataTaskWithRequest(request: NSURLRequest, completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void) -> NSURLSessionDataTask? {
+        
+        return SessionNoopTask(completion: { () -> () in
+            completionHandler(NSData(), nil, nil)
+        })
+    }
 }
